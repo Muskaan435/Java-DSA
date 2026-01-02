4b5bcb96f58f75693f93bd36c0b67fc9fd09abd0
Muskaan435 <muskaanpichode@gmail.com>
2026-01-02 21:34:48 +0530

    Variables and Datatypes

---
+++ Variables And Datatypes/product.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class product {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int first = sc.nextInt();
+        int second = sc.nextInt();
+        int product = first*second;
+        System.out.println(product);
+        sc.close();
+
+    }
+}
