8ed3d419b7b9001719d22edfc6ba69ca3820533f
Muskaan435 <muskaanpichode@gmail.com>
2026-01-04 22:29:18 +0530

    learnt arithmatic and unary operators

---
+++ Operators/Arithmatic.java
@@ -0,0 +1,11 @@
+public class Arithmatic {
+    public static void main(String[] args) {
+        int a = 25;
+        int b = 15;
+        System.out.println("Addition is : " +(a+b));
+        System.out.println("Subtraction is : " +(a-b));
+        System.out.println("Multiplication is : " +(a*b));
+        System.out.println("Division is : " +(a/b));
+        System.out.println("Modulo(Remainder) is : " +(a%b));
+    }
+}
---
+++ Operators/Unary.java
@@ -0,0 +1,10 @@
+public class Unary {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a);//pre increment
+        a = 10;
+        int b = a++;// post incerement
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
