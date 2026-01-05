1a8872306bfc0e3e48ffd72f6b9ceef320efcb8e
Muskaan435 <muskaanpichode@gmail.com>
2026-01-05 09:28:28 +0530

    understad assignment, logical and relationnal operators

---
+++ Operators/Assignment.java
@@ -0,0 +1,13 @@
+public class Assignment {
+    public static void main(String[] args) {
+        int a = 10; //assign
+        a+=10; // a = a + 10 i.e 20
+        System.out.println(a);
+        a -= 5; // a = a - 15 i.e 15 because previously a's value was 20
+        System.out.println(a);
+        a *= 3; //a = a * 3 i.e 45
+        System.out.println(a);
+        a/= 15; //a = a / 15 i.e 3
+        System.out.println(a);
+    }
+}
---
+++ Operators/Logical.java
@@ -0,0 +1,7 @@
+public class Logical {
+    public static void main(String[] args) {
+        System.out.println(12>3 && 15>2); //both true -> true
+        System.out.println(5>12 || 12 != 11); // one true -> true
+        System.out.println(!true); //false -> true and true -> false
+    }
+}
