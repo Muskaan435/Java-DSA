84701da64bd0b37637fc838bd4f4b9184ed7ca42
Muskaan435 <muskaanpichode@gmail.com>
2026-01-21 18:16:13 +0530

    started with functions

---
+++ FunctionsAndMethods/BasicFunc.java
@@ -0,0 +1,11 @@
+package FunctionsAndMethods;
+// whenever we call the function printHello, it will print "Hello World!" once.
+public class BasicFunc {
+    public static void printHello() {
+        System.out.println("Hello World!");
+    }
+    public static void main(String[] args) {
+        printHello();
+    }
+    
+}
