0b8143d21f44cdd653214104a85c8cd8d806e49f
Muskaan435 <muskaanpichode@gmail.com>
2026-01-30 09:16:55 +0530

    add two and three number with same fn name

--- FunctionsAndMethods/MultiFunctions.java
+++ FunctionsAndMethods/MultiFunctions.java
@@ -1,10 +1,11 @@
 package FunctionsAndMethods;
 import java.util.*;
 public class MultiFunctions {
+    // function to calculate product of two numbers
     public static int product(int a, int b){
         return a*b;
     }
-
+    // function to calculate factorial of a number
     public static int factorial(int a){
         int fact = 1;
         for(int i =2;i<=a;i++){
@@ -12,11 +13,21 @@ public class MultiFunctions {
         }
         return fact;
     }
+    // function to calculate nCr
     public static int coofficient(int n,int r){
         int nuem = factorial(n);
         int denm = factorial(r) * factorial(n-r);
         int coef = nuem/denm;
         return coef;
+    }
+    // function to calculate sum of two numbers
+    public static int sum(int a,int b){
+        return a+b;    
+    }
+
+    //function to calculate sum of three numbers
+    public static int sum(int a, int b, int c){
+        return a+b+c;
     }
     
     public static void main(String[] args) {
@@ -29,5 +40,7 @@ public class MultiFunctions {
         System.out.println("product is : " +product(a,b));
         System.out.print(factorial(5));
         System.out.print(coofficient(7, 3));
+        System.out.println(sum(10,25));
+        System.out.println(sum(10,25,30));
     }
 }
